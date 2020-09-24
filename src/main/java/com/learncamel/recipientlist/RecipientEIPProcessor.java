7dae098a5555160f36eab368a4fb830986f9acd1
JamieGoodnight <jamiegoodnight3@gmail.com>
2020-09-24 13:18:30 -0400

    :sparkles: RecipientList route complete

--- src/main/java/com/learncamel/recipientlist/RecipientEIPProcessor.java
+++ src/main/java/com/learncamel/recipientlist/RecipientEIPProcessor.java
@@ -8,9 +8,12 @@ public class RecipientEIPProcessor implements org.apache.camel.Processor {
         String employeeType =
                 exchange.getIn().getHeader("type", String.class);
         System.out.println("~~~~~~~~~~ EXCHANGE HEADER BELOW ~~~~~~~~~");
-        System.out.println(exchange.getIn());
+        System.out.println(exchange.getIn().getBody(String.class));
+        System.out.println(exchange.getIn().getHeaders());
+        System.out.println(exchange);
         if (employeeType.equals("senior")) {
             exchange.getIn().setHeader("type", "file:xmlsenior");
+            System.out.println(exchange.getIn().getHeaders());
         } else {
             exchange.getIn().setHeader("type", "file:xmljunior");
         }
